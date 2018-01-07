8e79b8b76b2c746d4e6aa2fe206d4a222a6be6b6
Ambar Agrawal <acxelense@gmail.com>
2018-01-07 14:31:41 +0545

    Initial Commit

---
+++ application/views/base.php
@@ -0,0 +1,11 @@
+<!-- SCRIPTS -->
+<!-- JQuery -->
+<script type="text/javascript" src="assets/plugin/js/jquery-3.2.1.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="assets/plugin/js/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="assets/plugin/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="assets/plugin/js/mdb.min.js"></script>
+</body>
+</html>
---
+++ application/views/head.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en" class="full-height">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Material Design Bootstrap</title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Bootstrap core CSS -->
+  <link href="assets/plugin/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="assets/plugin/css/mdb.min.css" rel="stylesheet">
+  <!-- Your custom styles (optional) -->
+  <link href="assets/css/styles.css" rel="stylesheet">
+</head>
+<body>
